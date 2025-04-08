d39848ae9743b446f84a30b78263e856db20121d
Daniel Manrique <daniel.manrique@fly.io>
2025-04-08 17:38:38 -0400

    schema update

--- restarter/schema.sql
+++ restarter/schema.sql
@@ -7,7 +7,7 @@ frequency INTEGER NOT NULL,
 expires_at INTEGER NOT NULL,
  api_key TEXT NOT NULL,
 last_check TEXT
-)
+);
 
 create index if not exists idx_apikey_slug on monitor(api_key, slug);
 create index if not exists idx_expires_at on monitor(expires_at);
